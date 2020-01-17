a207637462fdf8a3114ade387002a71040e3b2f2
unknown <mpritchett516@gmail.com>
2020-01-17 16:54:56 -0500

    added material-ui and material-ui-icons dependencies and styled app and added new functionality to the create note component with new dependencies

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,7 +1,11 @@
 import React, {useState} from "react";
-import { addSyntheticTrailingComment } from "typescript";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+
+    const [isExpanded, setExpanded] = useState(false);
 
     const [note, setNote] = useState({
         title: '',
@@ -25,15 +29,39 @@ function CreateArea(props) {
         });
     }
 
+    function submitNote(event) {
+        props.onAdd(note);
+        setNote({
+            title: '',
+            content: ''
+        });
+        event.preventDefault();
+    }
+
+    function expand() {
+        setExpanded(true);
+    }
+
     return (
         <div>
-        <form>
-            <input onChange={handleChange} name="title" placeholder="Title" value={note.title}/>
-            <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={note.content}/>
-            <button onClick={(event) => {
-                props.onAdd(note);
-                event.preventDefault();
-            }}>Add</button>
+        <form className="create-note">
+            {isExpanded && <input
+                onChange={handleChange}
+                name="title"
+                placeholder="Title"
+                value={note.title}
+            />}
+            <textarea
+                onClick={expand}
+                onChange={handleChange}
+                name="content"
+                placeholder="Take a note..."
+                rows={isExpanded ? '3' : '1'}
+                value={note.content}
+            />
+            {isExpanded && <Zoom in={true}>
+                <Fab onClick={submitNote}><AddIcon /></Fab>
+            </Zoom>}
         </form>
         </div>
     );
